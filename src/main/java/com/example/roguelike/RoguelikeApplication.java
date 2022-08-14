606c82e3ba39e9bb4dd13225d919fd703dddf68d
tyangpark <tyangpark@gmail.com>
2022-08-14 16:54:02 +0900

    #1

---
+++ src/main/java/com/example/roguelike/RoguelikeApplication.java
@@ -0,0 +1,13 @@
+package com.example.roguelike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoguelikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoguelikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/roguelike/RoguelikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roguelike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoguelikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
